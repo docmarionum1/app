56ff1b32049d337df7347539265826882485cefe
Jeremy Neiman <docmarionum1@gmail.com>
2016-05-21 19:37:46 -0400

    fixing bug with logging on older phones

--- www/js/logging.js
+++ www/js/logging.js
@@ -6,7 +6,7 @@ angular
         _scope: null,
 
         log: function(message) {
-            this._log += message + "\n";
+            this._log += message + "<br/>";
 
             if (this._scope && !this._scope.$$phase) {
                 this._scope.$apply();
