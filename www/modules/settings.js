9a58e208525de7f556f97c43317a3698a684d299
Jeremy Neiman <docmarionum1@gmail.com>
2016-07-18 12:49:57 -0400

    working on docs

--- www/modules/settings.js
+++ www/modules/settings.js
@@ -1,21 +1,28 @@
-// List of App Settings
+// Controls loading and saving app settings.
+
 var settings = {
+    // Whether to enable or disable scanning for wifi.
     'enableScanning': {
         type: 'boolean',
         default: true
     },
+    // Whether to upload the user's phone's mac address when storing wifi data.
     'uploadMacAddress': {
         type: 'boolean',
         default: false
     },
+    // Debugging mode enables the log
     'debug': {
         type: 'boolean',
         default: false
     },
+    // How intense to poll - either "low", "medium" or "high".
+    // See [scanning](scanning.html#section-37) for details.
     'pollingIntensity': {
         type: 'select',
         default: 'medium'
     },
+    // Whether to start the app automatically when the phone boots up.
     'autostart': {
         type: 'boolean',
         default: false
@@ -28,10 +35,13 @@ angular.module('WiFind.app')
         $rootScope.settings = {};
         var prefs = plugins.appPreferences;
 
+        // For each of the settings, try to load it.
         for (var key in settings) {
             (function(key) {
                 prefs.fetch(key).then(function(value) {
                     if (value === null) {
+                        // When the setting is currently null, use the default
+                        // value.
                         $rootScope.settings[key] = settings[key]['default'];
                     } else {
                         $rootScope.settings[key] = value;
@@ -40,6 +50,7 @@ angular.module('WiFind.app')
                     $rootScope.settings[key] = settings[key]['default'];
                 });
 
+                // Watch for changes to settings and store the new values.
                 $rootScope.$watch('settings.' + key, function() {
                     prefs.store(key, $rootScope.settings[key]);
                     logger.log(key + '=' + $rootScope.settings[key]);
